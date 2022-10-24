f8b1fb4012cd20c7679ba719b3f8476df000059c
EC2 Default User <amrumineq@gmail.com>
2022-10-24 02:13:31 +0000

    errorsy修正

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,20 @@
 class BookCommentsController < ApplicationController
   def create
-    book =Book.find(params[:book_id])
+    @book =Book.find(params[:book_id])
     comment =current_user.book_comments.new(book_comment_params)
-    comment.book_id= book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    comment.book_id= @book.id
+    if comment.save
+       redirect_to book_path(@book)
+    else 
+      @booknew =Book.new
+      @user =@book.user
+      @book_comment = comment
+      render "books/show"
+    end
   end
 
   def destroy
-    book =Book.find(params[:book_id])
-    comment =current_user.book_comments.find_by(book_id:book.id)
-    comment.destroy
+    BookComment.find(params[:id]).destroy
     redirect_back(fallback_location: root_path)
   end
 
